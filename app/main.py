162f15abb4889158dd1caf25e59416aa89c9a206
Yuzi <yuzvendhrachahal@gmail.com>
2026-09-04 10:24:14 +0000

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import client, init_db
 from app.routers.auth import router as auth_router
@@ -24,6 +25,15 @@ app = FastAPI(
 app.include_router(users_router)
 app.include_router(auth_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
